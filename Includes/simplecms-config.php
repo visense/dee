304527a6cb1c8b0341ee282fa7072a8243a28b5c
visense zh <visenselx@hotmail.com>
2015-09-27 20:58:31 +0800

    downloaded

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'simplecms726');
+    define('DB_USER', 'simplecmsuser726');
+    define('DB_PASSWORD', 'u6a#|0E#m-1X');
+    define('DB_HOST', 'localhost');
+
+    define('DEFAULT_ADMIN_USERNAME', 'admin');
+    define('DEFAULT_ADMIN_PASSWORD', 'admin');
+?>
